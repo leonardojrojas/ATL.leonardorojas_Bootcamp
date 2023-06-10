ab86b02269a3eefb88df54c8001e2a8373b40d83
Leonardo Rojas <33873409+leonardojrojas@users.noreply.github.com>
2023-06-10 13:31:27 -0500

    Update question.js

--- question.js
+++ question.js
@@ -1,7 +1,7 @@
 {
-    "category" : "Cultura",
-    "question" : "¿Qué ingredientes se necesitan para hacer una michelada?",
-    "options" : ["Cerveza, lima/limón, sal, chile en polvo, salsa tipo inglesa, picante", "Cerveza, lima/limón y limon", "Cerveza, sal, tabasco y lima/limón"],
-    "answer" : 0,
-    "explanation": "La michelada, originaria de México, nacida de la creatividad y el deseo de refrescar, se convirtió en una popular bebida que cautivó a los amantes de la cerveza con su combinación única de sabores y su espíritu festivo."
+    "category" : "Protocolos",
+    "question" : "¿Cual es el protocolo de comunicacion mas popular para edifificos inteligentes?",
+    "options" : ["Modbus/TPC", "Modbus/RTU", "Bacnet","KNX", "Lon" ],
+    "answer" : 2,
+    "explanation": "El protocolo BACnet tiene en su haber mas de 30 años de desarrollo continuo y es ampliamente utilizado en el mundo entero, ademas cuenta con multiples medios de conexion fisica."
 }
