a5086285a8c5b2a92826342679e12395f15a0270
lrojas <leonardo.jrojas@gmail.com>
2023-06-17 14:53:37 -0500

    Actividades_Bootcamp_Clase6

---
+++ Clase_06/Conteo100.java
@@ -0,0 +1,14 @@
+package Clase_06;
+
+import java.util.Scanner;
+
+public class Conteo100 {
+    public static void main(String[] arg) {
+        System.out.println("El siguiente programa muestra los numeros del 1 al 100");
+        int contador = 0;
+        while (contador < 100) {
+            contador++;
+            System.out.println("contador: " + contador);
+        }
+    }
+}
